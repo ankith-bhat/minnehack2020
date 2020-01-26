77e0c9aa806c1a6740bb01970daa02c53a913e2c
Karanpadhu1 <padhukaran@gmail.com>
2020-01-25 19:43:58 -0600

    amith user

--- src/User.java
+++ src/User.java
@@ -2,13 +2,30 @@ import java.util.ArrayList;
 
 public class User {
 
-ArrayList <String> interests = new ArrayList <String> ();
+	protected ArrayList <String> interests = new ArrayList <String> ();
+
+	 protected String name;
+	 
+	 
+	 User curFriend = null;
+	 
+	public User()
+	{
+		
+	}
 
 
-	User()
-{
-  
-}
-
+	/*TODO: Get name from front end and enter in for user.
+	 * 
+	 * public void setname() {
+	
+	System.out.println("Please enter your name friend");
+	
+	System.in.
+	
+	}*/
+	
+	
+	
 
 }
