08986da12d78733571b11535185756bf4ac348d3
BhatAmith <59381100+BhatAmith@users.noreply.github.com>
2020-01-26 10:32:28 -0600

    Updated friendr

--- src/friendr.java
+++ src/friendr.java
@@ -18,7 +18,7 @@ public class friendr {
 		interests2.add("Gaming");
 		interests1.add("Poker");
 		interests2.add("Poker");
-		interests1.add("Blacking Out");
+		interests1.add("Gains");
 		interests2.add("Test");
 		interests3.add("Nothing");
 		
@@ -31,7 +31,7 @@ public class friendr {
 		karan.friendSearch(karan);
 		System.out.println(karan.toStringFriend());
 		ankith.friendSearch(ankith);
-		System.out.println(ankith.toStringFriend());
+		System.out.println(ankith.toStringFriend());0
 		
 		
 	
